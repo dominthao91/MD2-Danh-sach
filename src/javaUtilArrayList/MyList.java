422da919900dfdaaffdcb5ee150cb8c6fd03a0f7
dominthao91 <domintaopt@gmail.com>
2021-08-18 15:22:46 +0700

    update

--- src/javaUtilArrayList/MyList.java
+++ src/javaUtilArrayList/MyList.java
@@ -2,27 +2,100 @@ package javaUtilArrayList;
 
 import java.util.Arrays;
 
-public class MyList<T>{
-    private int size = 0;
-    private int DEFAULF_CAPACITY = 10;
-    private Object elements[];
-    public MyList(){
-        elements = new Object[DEFAULF_CAPACITY];
+public class MyList{
+    private int size = 10;
+    private int[] elements;
+    private int count = 0;
+
+    public MyList() {
+        elements = new int[size];
     }
-    private void ensureCapa() {
-        int newSize = elements.length * 2;
-        elements = Arrays.copyOf(elements, newSize);
+
+    public int[] getElements() {
+        return elements;
     }
-    public void add(T t) {
-        if (size == elements.length) {
-            ensureCapa();
+
+    public void setElements(int[] elements) {
+        this.elements = elements;
+    }
+
+    private void ensureCapacity() {
+        int newSize = size * 2;
+        Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(int index, int value) {
+        if (count == size) {
+            ensureCapacity();
         }
-        elements[size++] = t;
-    }
-    public T get(int i) {
-        if (i>= size || i <0) {
-            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
+        int[] newArray = new int[elements.length + 1];
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < index) {
+                newArray[i] = elements[i];
+            } else if (i == index) {
+                newArray[index] = value;
+            } else {
+                newArray[i] = elements[i - 1];
+            }
         }
-        return (T) elements[i];
+        elements = newArray;
+        this.count++;
+    }
+
+    public void remove(int index) {
+        int[] newArray = new int[elements.length - 1];
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < index) {
+                newArray[i] = elements[i];
+            } else {
+                newArray[i] = elements[i + 1];
+            }
+        }
+        elements = newArray;
+        this.count--;
+    }
+
+    public int size() {
+        return this.count;
+    }
+
+    public MyList clone() {
+        MyList myList = new MyList();
+        myList.count = this.count;
+        myList.size = this.size;
+        myList.elements = this.elements;
+        return myList;
+    }
+
+    public boolean contains(int value) {
+        boolean isContain = false;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == value) {
+                isContain = true;
+                break;
+            }
+        }
+        return isContain;
+    }
+
+    public int indexOf(int value) {
+        int index = -1;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == value) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public void clear() {
+        size = 10;
+        count = 0;
+        elements = new int[size];
+    }
+
+    public int get(int index) {
+        return elements[index];
     }
 }
--- src/javaUtilArrayList/MyListTest.java
+++ src/javaUtilArrayList/MyListTest.java
@@ -2,18 +2,22 @@ package javaUtilArrayList;
 
 public class MyListTest {
     public static void main(String[] args) {
-        MyList<Integer> listInteger = new MyList<Integer>();
-        listInteger.add(1);
-        listInteger.add(2);
-        listInteger.add(3);
-        listInteger.add(3);
-        listInteger.add(4);
-
-        System.out.println("element 4: " + listInteger.get(4));
-        System.out.println("element 1: " + listInteger.get(1));
-        System.out.println("element 2: " + listInteger.get(2));
-
-        listInteger.get(-1);
-        System.out.println("element -1: " + listInteger.get(-1));
+        MyList myList = new MyList();
+        myList.add(0, 1);
+        myList.add(0, 2);
+        myList.add(1, 0);
+        for (int x : myList.getElements()) {
+            System.out.println(x);
+        }
+        System.out.println("-------------------");
+        myList.remove(1);
+        for (int x : myList.getElements()) {
+            System.out.println(x);
+        }
+        System.out.println("--------------------");
+        System.out.println(myList.size());
+        System.out.println("--------------------");
+        MyList coppy = myList.clone();
     }
+
 }
